85d1ab1cffc10b9653f44acc842112e80cea28ee
iamtabrezalam <tabrezalam188@gmail.com>
2024-07-20 12:09:22 +0530

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 1222
+let accountPassword = "Tabrez123"
+var accountEmail = "tabrezalam188@gmail.com"
+accountcity = 2
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword])
+
+alert("Danger");
+let name = "Tabrez"
+let age = 25
+let logedId = true
+let floatNum = 56.6
+
+
+console.log(name)
